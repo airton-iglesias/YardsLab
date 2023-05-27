c1c6941debdac6359f167e07b28ddf539b3ceb85
airton-iglesias <airton.iglesias.3@hotmail.com>
2023-05-27 19:18:25 -0400

    YardsLab v0

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -153,7 +153,7 @@ export default function Header(pathname) {
                                         </Menu.Item>
                                         <Menu.Item>
                                             {({ active }) => (
-                                                <button onClick={cookiesLogout} className={classNames(active ? 'bg-blue-500' : '', 'block px-4 py-2 rounded-lg text-sm text-gray-700')}>Sair</button>)}
+                                                <Link href="#" className={classNames(active ? 'bg-blue-500' : '', 'block px-4 py-2 rounded-lg text-sm text-gray-700')}>Sair</Link>)}
                                         </Menu.Item>
                                     </Menu.Items>
                                 </Transition>
