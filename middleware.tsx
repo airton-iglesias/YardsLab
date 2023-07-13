33454a5c352fa24bd82b0fcdd77d7f91535f596b
airton-iglesias <airton.iglesias.3@hotmail.com>
2023-07-13 17:47:26 -0400

    YardsLab V0

--- middleware.tsx
+++ middleware.tsx
@@ -1,12 +1,2 @@
 import {NextRequest, NextResponse} from "next/server";
 
-
-export function middleware(req: NextRequest){
-    if(req.nextUrl.pathname.length > 1){
-        return NextResponse.rewrite(new URL('/comingsoon', req.url))
-    }
-    else{
-        return NextResponse.next();
-    }
-}
-
